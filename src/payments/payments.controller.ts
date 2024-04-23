b32ad39b2ce8b4f787bc952f9f61480ea1cd707a
BetoNajera <ing.betonajera@gmail.com>
2024-04-23 01:02:56 -0600

    :sparkles: Feat: Update PaymentsController and PaymentsService
    Update createPaymentsSession controller and uodate stripeWebhook in PaymentsService

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { Request, Response } from 'express';
 
 import { PaymentsService } from './payments.service';
@@ -6,10 +7,11 @@ import { PaymentSessionDto } from './dto';
 
 @Controller('payments')
 export class PaymentsController {
-  constructor(private readonly paymentsService: PaymentsService) { }
+  constructor(private readonly paymentsService: PaymentsService) {}
 
-  @Post('create-payment-session')
-  createPaymentsSession(@Body() paymentSessionDto: PaymentSessionDto) {
+  // @Post('create-payment-session')
+  @MessagePattern('create.payment.session')
+  createPaymentsSession(@Payload() paymentSessionDto: PaymentSessionDto) {
     return this.paymentsService.createPaymentSession(paymentSessionDto);
   }
 
