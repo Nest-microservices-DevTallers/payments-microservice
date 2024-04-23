b12181d4f398d011236179d9c759b8495ea17d68
BetoNajera <ing.betonajera@gmail.com>
2024-04-22 20:43:27 -0600

    :tada: Begin: Start project
    Start project

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const logger = new Logger();
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(3000, () => {
+    logger.log(`🚀 Payments microservice running on port ${3000} `);
+  });
+}
+bootstrap();
